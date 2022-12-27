402423fe4198c9bb65bb5b11a6a575eaa5ad65a8
Vaniones <112714357+Vaniones@users.noreply.github.com>
2022-12-27 15:14:49 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ import pytest
 
 from blender_ml_dev.models.vaniones import DEFAULTS as VANIONES_DEFAULTS
 from blender_ml_dev.models.vaniones import Vaniones
-from uberduck_ml_dev.trainer.vaniones import (
+from blender_tests_ml_dev.trainer.vaniones import (
     VanionesTrainer,
     DEFAULTS as VANIONES_TRAINER_DEFAULTS,
 )
